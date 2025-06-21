5c6ee06fbe9c6ef0c0fbb251a84b563dc14524f9
mouhazba <bamahadiou@gmail.com>
2025-06-21 14:43:26 -0400

    some updates from my .py and .html files

--- welcome.py
+++ welcome.py
@@ -1,6 +1,9 @@
+def welcome_func(name):
+    if name == "Mamadou":
+        print("Hey, it's the awesome AI Director, Mamadou!")
+    else:
+        print(f"Welcome, {name}! We're glad to have you here.")
+    
 name = input("Enter your name: ")
 
-if name == "Mamadou":
-    print("Hey, it's the awesome AI Director, Mamadou!")
-else:
-    print(f"Welcome, {name}! We're glad to have you here.")
+welcome_func(name)
